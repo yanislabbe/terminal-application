7debd1bf0bc13b429c113e64ae59f30f00f75c03
yanislabbe <labbeyanis@icloud.com>
2023-05-12 19:21:52 -0400

    added command history when using up and down arrows

--- public/script.js
+++ public/script.js
@@ -37,14 +37,43 @@ function executeCommand(command) {
 
 document.addEventListener('DOMContentLoaded', function() {
     const input = document.getElementById('commandInput');
+    let commandHistory = [];
+    let historyIndex = -1;
+    let lastCommand = '';
+
     input.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
+        if (event.key === 'ArrowUp') {
+            event.preventDefault();
+            if (commandHistory.length > 0 && historyIndex < commandHistory.length - 1) {
+                historyIndex++;
+                input.value = commandHistory[commandHistory.length - 1 - historyIndex];
+            } else {
+                input.value = lastCommand;
+            }
+        } else if (event.key === 'ArrowDown') {
+            event.preventDefault();
+            if (historyIndex >= 0) {
+                historyIndex--;
+                input.value = (historyIndex >= 0) ? commandHistory[commandHistory.length - 1 - historyIndex] : lastCommand;
+            } else {
+                input.value = '';
+            }
+        } else if (event.key === 'Enter') {
             event.preventDefault();
             const command = input.value.trim();
             input.value = '';
-            writeToTerminal(`$ ${command}`);
-            executeCommand(command);
+            if (command !== '') {
+                commandHistory.push(command);
+                lastCommand = command;
+                historyIndex = -1;
+                writeToTerminal(`$ ${command}`);
+                executeCommand(command);
+            }
         }
     });
+
     input.focus();
 });
+
+
+
